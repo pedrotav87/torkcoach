25dd7361da1520ac0b96275a2da93df24b5b9437
pedrotav87 <pedrotav@outlook.com>
2025-12-22 17:27:42 +0000

    Edited Spark

--- src/ErrorFallback.tsx
+++ src/ErrorFallback.tsx
@@ -1,29 +1,29 @@
 import { Alert, AlertTitle, AlertDescription } from "./components/ui/alert";
-import { Button } from "./components/ui/button";
+import { ArrowClockwise } from "@phosphor-icons/
 import { ArrowClockwise } from "@phosphor-icons/react";
 
-interface ErrorFallbackProps {
-  resetErrorBoundary: () => void;
-}
+export default function ErrorF
+    <div className="w-full max-w-
+ 
 
-export default function ErrorFallback({ resetErrorBoundary }: ErrorFallbackProps) {
-  return (
-    <div className="w-full max-w-md mx-auto p-4">
-      <Alert variant="destructive">
-        <AlertTitle>Something went wrong</AlertTitle>
-        <AlertDescription>
-          An unexpected error occurred. You can try again.
-        </AlertDescription>
       </Alert>
-
-      <Button
-        onClick={resetErrorBoundary}
+      <But
         className="w-full mt-4"
-      >
-        <ArrowClockwise className="w-4 h-4 mr-2" />
-        Retry
+        <ArrowClockwise className="
       </Button>
-    </div>
   );
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
